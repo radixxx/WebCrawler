f5b62aa888269e6b80332a3e53dfdb7b3ecd3478
radixxx <radov.2011@gmail.com>
2018-03-22 01:48:23 +0200

    first commit, is added jsoup library in project

---
+++ src/WebCrawler.java
@@ -0,0 +1,15 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+
+public class WebCrawler {
+
+
+
+
+}
